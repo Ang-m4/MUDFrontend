78d319560fa2eeac432396099b7b4829590e4f44
Andres <giraldoma@javeriana.edu.co>
2022-05-12 20:09:52 -0500

    Game Secton added

--- src/app/Game/game-view/game-view.component.ts
+++ src/app/Game/game-view/game-view.component.ts
@@ -1,4 +1,7 @@
+import { MonsterService } from 'src/app/Shared/monster.service';
 import { Component, OnInit } from '@angular/core';
+import { Room } from 'src/app/model/room';
+import { RoomService } from 'src/app/Shared/room.service';
 
 @Component({
   selector: 'app-game-view',
@@ -7,8 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameViewComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {}
+  roomSelected: Room = new Room(-1,"");
+
+  constructor(private roomService: RoomService) { }
+
+  ngOnInit(): void {
+
+    if(this.roomSelected.id == -1){
+      this.pickRoom()
+    }
+
+    console.log(this.roomSelected.name);
+
+  }
+
+  pickRoom(){
+
+    this.roomService.findAll().subscribe(list => {
+      this.roomSelected = list[this.getRandom(list.length)]
+    });
+
+  }
+
+   getRandom(max: number){
+    return Math.floor(Math.random() * Math.floor(max))
+  }
 
 }
